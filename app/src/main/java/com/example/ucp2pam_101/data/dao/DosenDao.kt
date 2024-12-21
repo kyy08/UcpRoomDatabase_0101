031aa0478549a8de050b593fa7d8f68708bdbe49
kyy08 <kyyelzz1@gmail.com>
2024-12-22 01:03:04 +0700

    menambahkan get AllDosen dan get Dosen di DosenDao

--- app/src/main/java/com/example/ucp2pam_101/data/dao/DosenDao.kt
+++ app/src/main/java/com/example/ucp2pam_101/data/dao/DosenDao.kt
@@ -2,7 +2,9 @@ package com.example.ucp2pam_101.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.ucp2pam_101.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface DosenDao {
@@ -10,5 +12,11 @@ interface DosenDao {
     suspend fun insertDosen(
         dosen: Dosen
     )
+
+    @Query("SELECT * FROM dosen ORDER BY nama ASC")
+    fun getAllDosen(): Flow<List<Dosen>>
+
+    @Query("SELECT * FROM dosen WHERE nidn = :nidn")
+    fun getDosen(nidn: String): Flow<Dosen>
 
 }
